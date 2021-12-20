5158c03fe0c401ea4b72229fa3ed7f435ae7dde2
ChubatovaTiger <78346593+ChubatovaTiger@users.noreply.github.com>
2021-12-20 17:35:53 +0100

    Create nUNitWithDisplayName.cs

---
+++ TestProject2/nUNitWithDisplayName.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace TestProject2
+{
+    public class NUnitTest1wthDisplN
+    {
+
+
+[TestCase(5,TestName="Test Case #1, Category: First, Category: Second")]
+public void TestCase() {
+    Assert.AreEqual(5, 4);
+}
+
+        
+        
+        
+    }
+}
