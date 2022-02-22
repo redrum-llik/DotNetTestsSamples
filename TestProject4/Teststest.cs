ffc69939f1c7deb618ca6ce90c0fc308038b01c2
ChubatovaTiger <78346593+ChubatovaTiger@users.noreply.github.com>
2022-02-22 14:36:19 +0100

    Update Teststest.cs

--- TestProject4/Teststest.cs
+++ TestProject4/Teststest.cs
@@ -1,47 +1,19 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Diagnostics.Contracts;
-using Xunit;
-
+using NUnit.Framework;
 
 namespace TestProject4
 {
-    public class dsplNameXunit11
+    public class UnitTest1
     {
-        //[Fact(DisplayName = "dpNameChubatova1")]
-        public void Test1chubatova()
-        {
-            Assert.Equal("a", "b");
-        }
 
-         [Theory(DisplayName = "dpNameChubatova2")]
-        [InlineData("a")]
-        [InlineData("b")]
-        [InlineData("c")]
-        [InlineData("d")]
-        public void Test2chubatova(string str)
+
+        [Test]
+ 
+        public void Test1NunitSimplever1()
         {
-            Assert.Equal("a", "a");
+            Assert.Fail();
         }
+        
+        
+        
     }
-    
-    
-    /*public class dsplNameXunit21
-    {
-        //[Fact(DisplayName = "dpNameChubatova3")]
-        public void Test1chubatova()
-        {
-        }
-
-         [Theory(DisplayName = "dpNameChubatova4")]
-        [InlineData("a")]
-        [InlineData("b")]
-        [InlineData("c")]
-        [InlineData("d")]
-        public void Test2chubatova(string str)
-        {
-            Assert.Equal(str, "a");
-        }
-    }*/
 }
