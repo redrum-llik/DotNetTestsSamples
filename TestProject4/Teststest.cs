27e84158a1aa6fbeb13f225d2fd33344ad2e4888
ChubatovaTiger <78346593+ChubatovaTiger@users.noreply.github.com>
2022-02-22 14:25:19 +0100

    Update Teststest.cs

--- TestProject4/Teststest.cs
+++ TestProject4/Teststest.cs
@@ -7,16 +7,41 @@ using Xunit;
 
 namespace TestProject4
 {
-    public class dsplNameXunit1
+    public class dsplNameXunit11
     {
-        
+        [Fact(DisplayName = "dpNameChubatova1")]
         public void Test1chubatova()
         {
             Assert.Equal("a", "b");
         }
 
-        
+        [Theory(DisplayName = "dpNameChubatova2")]
+        [InlineData("a")]
+        [InlineData("b")]
+        [InlineData("c")]
+        [InlineData("d")]
+        public void Test2chubatova(string str)
+        {
+            Assert.Equal("a", "a");
+        }
     }
     
     
+    public class dsplNameXunit21
+    {
+        [Fact(DisplayName = "dpNameChubatova3")]
+        public void Test1chubatova()
+        {
+        }
+
+        [Theory(DisplayName = "dpNameChubatova4")]
+        [InlineData("a")]
+        [InlineData("b")]
+        [InlineData("c")]
+        [InlineData("d")]
+        public void Test2chubatova(string str)
+        {
+            Assert.Equal(str, "a");
+        }
+    }
 }
