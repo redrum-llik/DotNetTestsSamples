1c628475adc95ba0cbfb452ad8149105f14277be
ChubatovaTiger <78346593+ChubatovaTiger@users.noreply.github.com>
2022-02-22 13:50:30 +0100

    Update Mstest.cs

--- TestProject3/Mstest.cs
+++ TestProject3/Mstest.cs
@@ -3,7 +3,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace TestProject3
 {
     [TestClass]
-    public class UnitTest1
+    public class UnitTest1_pr3
     {
         [TestMethod]
         public void TestMethod1()
