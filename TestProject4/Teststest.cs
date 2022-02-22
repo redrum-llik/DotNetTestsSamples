5138b3100311182ba38e299f3fba578f767390a0
ChubatovaTiger <78346593+ChubatovaTiger@users.noreply.github.com>
2022-02-22 14:15:18 +0100

    Create Teststest.cs

---
+++ TestProject4/Teststest.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Diagnostics.Contracts;
+
+
+
+namespace TestProject1
+{
+    public class Teststest
+    {
+
+        public void Test1chubatova()
+        {
+            Assert.Equal("a", "b");
+        }
+
+    }
+    
+    
+
+}
