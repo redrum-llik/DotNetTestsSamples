676b443b8c2293769e18f746ffd5511c4abff0f9
ChubatovaTiger <78346593+ChubatovaTiger@users.noreply.github.com>
2022-03-31 13:48:12 +0200

    Update Program.cs

--- TestProject4/Program.cs
+++ TestProject4/Program.cs
@@ -1,13 +1,12 @@
 using System;
 
 
-namespace TestProject4
-{
-    static class Program
+
+   public static class Program
     {
         static void Main()
         {
 Console.WriteLine("hihi");
         }
     }
-}
+
