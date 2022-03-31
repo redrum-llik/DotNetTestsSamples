44a6d511d34d0511f14d08dafda50a5d1089138d
ChubatovaTiger <78346593+ChubatovaTiger@users.noreply.github.com>
2022-03-31 13:37:32 +0200

    Create Program.cs

---
+++ TestProject4/Program.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Windows.Forms;
+
+namespace TestProject4
+{
+    static class Program
+    {
+        static void Main()
+        {
+Console.WriteLine("hihi");
+        }
+    }
+}
