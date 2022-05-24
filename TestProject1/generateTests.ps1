99966196dd0ac48d650960118ceaea9265374cae
ChubatovaTiger <78346593+ChubatovaTiger@users.noreply.github.com>
2022-05-24 19:25:37 +0200

    Rename 1.ps1 to generateTests.ps1

---
+++ TestProject1/generateTests.ps1
@@ -0,0 +1,5 @@
+﻿for (($i = 0); $i -lt 25000; $i++)
+{
+ $m='UnitTestUnitTestUnitTestUnitTestUnitTestUnitTestUnitTestUnitTestUnitTestUnitTestUnitTestUnitTestUnitTest' + $i;
+ (Get-Content .\xUnit.cs) -Replace 'UnitTest1', $m | Set-Content .\xUnit$i.cs
+}
