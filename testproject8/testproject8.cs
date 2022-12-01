b2ce4e299d6e02f48fd20d734f46d7e5b23e216b
ChubatovaTiger <78346593+ChubatovaTiger@users.noreply.github.com>
2022-12-01 11:30:14 +0100

    Create testproject8.cs

---
+++ testproject8/testproject8.cs
@@ -0,0 +1,21 @@
+using NUnit.Framework;
+using System.Threading;
+
+namespace TestProject8
+{
+    public class UnitTest1
+    {
+
+
+        [Test]
+ 
+        public void Test1NunitSimplever1()
+        {
+            Thread.Sleep(30);
+            Assert.Fail();
+        }
+        
+        
+        
+    }
+}
