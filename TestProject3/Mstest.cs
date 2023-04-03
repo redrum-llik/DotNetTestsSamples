055736ac01678e0eee2f1a2fffb954f87769dd53
AChubatova <n.chubatova@gmail.com>
2023-04-03 04:58:09 -0700

    ch1

--- TestProject3/Mstest.cs
+++ TestProject3/Mstest.cs
@@ -12,7 +12,7 @@ namespace TestProject3
         
         [DataTestMethod]
         [DataRow( "Aa", "gb")]
-        [DataRow( "Xx", "Ydf")]
+        [DataRow( "Xx", "Yfdf")]
         public void MSTestDataRowTestMethod2(string str1, string str2)
         {
         }
